5c899a7a6cf6eb856a1ffc5fbf5b8e7ffda7a2c2
sua <sua@DESKTOP-05VT1II>
2025-03-12 14:03:34 +0900

    초기 커밋.

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld //namespace 패키지 같은 거
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
